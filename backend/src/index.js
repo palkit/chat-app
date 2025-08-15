b867abb66db32a3fc0a9531167f53df2a681b112
palkit <dhiman.proff@gmail.com>
2025-08-15 13:29:43 +0530

    message M, C, R

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import cookieParser from "cookie-parser";
 dotenv.config();
 const app = express();
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes);
 
 connectDB().then(() => {
     app.listen(PORT, () => {
