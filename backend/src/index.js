0907100b1563f45ce4c5ade57519d3ac29411a56
palkit <dhiman.proff@gmail.com>
2025-08-17 21:10:45 +0530

    added pages lib store

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+import cors from "cors";
 import messageRoutes from "./routes/message.route.js";
 import cookieParser from "cookie-parser";
 dotenv.config();
@@ -11,6 +12,10 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors ({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes);
